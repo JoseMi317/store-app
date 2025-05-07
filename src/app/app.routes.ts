db212a53e7baca7d6af411112b2103e62f438b39
JoseMi317 <jmqc77@gmail.com>
2025-05-06 22:02:34 -0600

    fix errors and add a new componments and pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,44 @@
 import { Routes } from '@angular/router';
-import { CompranuevaComponent } from './pages/COMPRANUEVA/compranueva.component';
-import { facturacionComponent } from './pages/FACTURACION/facturacion.component';
+import { LoginComponent } from './pages/login/login.component';
+import { ComprasComponent } from './pages/compras/compras.component';
+import { ClientesComponent } from './pages/clientes/clientes.component';
+import { FacturasComponent } from './pages/facturas/facturas.component';
+import { HistorialComponent } from './pages/historial/historial.component';
+import { InventarioComponent } from './pages/inventario/inventario.component';
+import { ProveedoresComponent } from './pages/proveedores/proveedores.component';
+import { ReportesComponent } from './pages/reportes/reportes.component';
+import { VentasComponent } from './pages/ventas/ventas.component';
 
 export const routes: Routes = [
     {
-        path: 'compranueva', component: CompranuevaComponent
-  
-
+        path: 'login', component: LoginComponent
     },
+    {
+        path: 'compras', component: ComprasComponent
+    },
+    {
+        path: 'clientes', component: ClientesComponent
+    },
+    {
+        path: 'facturas', component: FacturasComponent
+    },
+    {
+        path: 'historial', component: HistorialComponent
+    },
+    {
+        path:'inventario', component: InventarioComponent
+    },
+    {
+        path: 'proveedores', component: ProveedoresComponent
+    },
+    {
+        path:'reportes', component: ReportesComponent
+    },
+    {
+        path: 'ventas', component: VentasComponent
+    },
+    {
+        path: '', redirectTo: 'login', pathMatch: 'full'
+    }
+
 ];
