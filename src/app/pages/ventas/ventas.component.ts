6f0ca4b5affde1e4cc24bd1706b943e53d9c898a
CesarEduardoSantizo <santizosantosf37@gmail.com>
2025-05-07 19:25:48 -0600

    Pagina de facturacion nueva venta

--- src/app/pages/ventas/ventas.component.ts
+++ src/app/pages/ventas/ventas.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; // ✅ necesario para usar *ngIf y más
 
 @Component({
   selector: 'app-ventas',
-  imports: [],
+  standalone: true, // opcional, si estás usando componentes standalone
+  imports: [CommonModule], // ✅ aquí se importa CommonModule
   templateUrl: './ventas.component.html',
-  styleUrl: './ventas.component.css'
+  styleUrls: ['./ventas.component.css']
 })
 export class VentasComponent {
+  mostrarNuevoCliente = false;
 
+  abrirFormularioCliente() {
+    this.mostrarNuevoCliente = true;
+  }
+
+  cerrarFormularioCliente() {
+    this.mostrarNuevoCliente = false;
+  }
 }
