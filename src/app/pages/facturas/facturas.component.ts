ba49fcf85a0b374bbf1c34e1b5752dee1d90005f
CesarEduardoSantizo <santizosantosf37@gmail.com>
2025-05-08 20:04:58 -0600

    pagina de facturas

--- src/app/pages/facturas/facturas.component.ts
+++ src/app/pages/facturas/facturas.component.ts
@@ -2,16 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
-interface Factura {
-  facturaNum: string;
-  cliente: string;
-  fecha: string;
-  vendedor: string;
-  neto: number;
-  iva: number;
-  total: number;
-}
-
 @Component({
   selector: 'app-facturas',
   standalone: true,
@@ -20,12 +10,11 @@ interface Factura {
   styleUrls: ['./facturas.component.css']
 })
 export class FacturasComponent {
-  facturas: Factura[] = [
-    { facturaNum: '888893', cliente: '', fecha: '07-05-2025', vendedor: 'Obed Alvarado', neto: 100.00, iva: 0.00, total: 100.00 },
-    { facturaNum: '888892', cliente: '', fecha: '06-05-2025', vendedor: 'Obed Alvarado', neto: 544.00, iva: 0.00, total: 544.00 },
-    { facturaNum: '888891', cliente: 'Lucía Nava', fecha: '04-05-2025', vendedor: 'Obed Alvarado', neto: 161432.00, iva: 0.00, total: 161432.00 },
-    { facturaNum: '888890', cliente: 'Javier Renteria', fecha: '02-05-2025', vendedor: 'Obed Alvarado', neto: 3505.00, iva: 0.00, total: 3505.00 }
-  ];
+  fechaInicio: string = '';
+  fechaFin: string = '';
+  clienteSeleccionado: string = '';
+  busqueda: string = '';
+  paginaActual = 1;
+registrosPorPagina = 10;
 
-  totalRegistros = this.facturas.length;
 }
